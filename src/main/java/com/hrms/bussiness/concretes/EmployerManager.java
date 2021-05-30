ca61dbdbebd6c9a61ff1666b2aeee01281b8ced0
Erkan Yıldırım <erkanyildirim28@gmail.com>
2021-05-30 10:26:11 +0300

    employer validation

--- src/main/java/com/hrms/bussiness/concretes/EmployerManager.java
+++ src/main/java/com/hrms/bussiness/concretes/EmployerManager.java
@@ -2,12 +2,10 @@ package com.hrms.bussiness.concretes;
 
 import com.hrms.bussiness.abstracts.EmployerService;
 import com.hrms.core.utilities.MessageBundle;
-import com.hrms.core.utilities.results.DataResult;
-import com.hrms.core.utilities.results.Result;
-import com.hrms.core.utilities.results.SuccessDataResult;
-import com.hrms.core.utilities.results.SuccessResult;
+import com.hrms.core.utilities.results.*;
 import com.hrms.dataAccess.abstracts.EmployerDao;
 import com.hrms.entities.concretes.Employer;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
@@ -29,7 +27,56 @@ public class EmployerManager implements EmployerService {
 
     @Override
     public Result add(Employer employer) {
+
+        Result result = isValidate(employer);
+
+        if(!result.isSuccess()){
+            return result;
+        }
+
         employerDao.save(employer);
         return new SuccessResult(MessageBundle.getMessageTr("employer.add"));
     }
+
+    public Result isValidate(Employer employer) {
+
+        StringBuilder message = new StringBuilder();
+
+        if(StringUtils.isEmpty(employer.getFirmName())){
+            message.append(MessageBundle.getMessageTr("employer.validation.firmname"));
+        }
+
+        if(StringUtils.isEmpty(employer.getWebSite())){
+            message.append(MessageBundle.getMessageTr("employer.validation.website"));
+        }
+
+        if(StringUtils.isEmpty(employer.getPerson().getEmail())){
+            message.append(MessageBundle.getMessageTr("employer.validation.email"));
+        }
+
+        if(StringUtils.isEmpty(employer.getPerson().getPassword())){
+            message.append(MessageBundle.getMessageTr("employer.validation.password"));
+        }
+
+        if(message.length()>0){
+            message.deleteCharAt(message.length()-1);
+            return new ErrorResult(message.toString());
+        }
+
+        String email =employer.getPerson().getEmail();
+        String website = employer.getWebSite();
+
+        String emailDomain =email.substring(email.indexOf("@")+1,email.indexOf("com")-1);
+        String websiteDomain = website.substring(website.indexOf("www")+4,website.indexOf("com")-1);
+
+        if(!emailDomain.equals(websiteDomain)){
+            message.append(MessageBundle.getMessageTr("employer.validation.domain"));
+            return new ErrorResult(message.toString());
+        }
+
+
+        return new SuccessResult(message.toString());
+    }
+
+
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,12 @@
-INSERT INTO "Persons" ("Id","Email","Password","TelNo") VALUES ("personSeq".nextval,'etiya@gmail.com','123','507532404');
-INSERT INTO "Persons" ("Id","Email","Password","TelNo") VALUES ("personSeq".nextval,'cardif@gmail.com','1234','507532405');
-INSERT INTO "Persons" ("Id","Email","Password","TelNo") VALUES ("personSeq".nextval,'turtelekom@gmail.com','1235','507532406');
+INSERT INTO "Persons" ("Id","Email","Password","TelNo") VALUES ("personSeq".nextval,'vodafon-info@vodafon.com','123','507532404');
+INSERT INTO "Persons" ("Id","Email","Password","TelNo") VALUES ("personSeq".nextval,'turkcell-info@turkcell.com','1234','507532405');
+INSERT INTO "Persons" ("Id","Email","Password","TelNo") VALUES ("personSeq".nextval,'turtelekom-info@turtelekom.com','1235','507532406');
 
-INSERT INTO "Employers" ("Id","FirmName","WebSite") VALUES ((select "Id" from "Persons" where  "Email"='etiya@gmail.com'),'Etiya','www.etiya.com');
-INSERT INTO "Employers" ("Id","FirmName","WebSite") VALUES ((select "Id" from "Persons" where  "Email"='cardif@gmail.com'),'Cardif', 'www.cardif.com');
-INSERT INTO "Employers" ("Id","FirmName","WebSite") VALUES ((select "Id" from "Persons" where  "Email"='turtelekom@gmail.com'),'Türktelekom', 'www.turktelekom.com');
+INSERT INTO "Employers" ("Id","FirmName","WebSite") VALUES ((select "Id" from "Persons" where  "Email"='vodafon-info@vodafon.com'),'Vodafon','www.vodafon.com');
+INSERT INTO "Employers" ("Id","FirmName","WebSite") VALUES ((select "Id" from "Persons" where  "Email"='turkcell-info@turkcell.com'),'Türkcell', 'www.turkcell.com');
+INSERT INTO "Employers" ("Id","FirmName","WebSite") VALUES ((select "Id" from "Persons" where  "Email"='turtelekom-info@turtelekom.com'),'Türktelekom', 'www.turktelekom.com');
 
 INSERT INTO "Positions" ("Id","Name") VALUES ("positionSeq".nextval,'Software Developer');
 INSERT INTO "Positions" ("Id","Name") VALUES ("positionSeq".nextval,'Software Architect');
+
+
