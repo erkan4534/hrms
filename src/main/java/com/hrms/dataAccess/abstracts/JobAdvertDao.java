7bb1d613244cab96af1e867eaae3beb10dcbfa74
Erkan Yıldırım <erkanyildirim28@gmail.com>
2021-06-01 18:24:07 +0300

    add jobAdvert

--- src/main/java/com/hrms/dataAccess/abstracts/JobAdvertDao.java
+++ src/main/java/com/hrms/dataAccess/abstracts/JobAdvertDao.java
@@ -1,6 +1,5 @@
 package com.hrms.dataAccess.abstracts;
 
-import com.hrms.core.utilities.results.DataResult;
 import com.hrms.entities.concretes.JobAdvert;
 import com.hrms.entities.dto.JobAdvertGetDto;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,4 +10,6 @@ public interface JobAdvertDao extends JpaRepository<JobAdvert,Long> {
 
    @Query("SELECT new com.hrms.entities.dto.JobAdvertGetDto (u.employer.firmName,u.position.name,u.openPositionsNumber,u.createDate,u.applyDeadline) FROM JobAdvert u")
    List<JobAdvertGetDto> getAllActivePositionJob();
+
+
 }
